02fe33d8d4b85eca025ed117a3c3972ca89cf55c
LanguidBasil <e.makunev@gmail.com>
2021-10-16 12:44:09 +1000

    Created Gun class

---
+++ Assets/Scripts/Core/Gun.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+using Project.Tools;
+
+namespace Project.Core
+{
+    public class Gun : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject projectilePrefab;
+        [SerializeField]
+        private int maxBullets;
+        [SerializeField]
+        private float reloadTime;
+
+        public Action OnFire;
+
+        private GameObjectPool bullets;
+        private float reloadTimer;
+
+        private void Awake()
+        {
+            bullets = new GameObjectPool(maxBullets, projectilePrefab);
+
+            OnFire += () => { reloadTimer = Time.time + reloadTime; };
+        }
+
+        public void Fire()
+        {
+            if (Time.time > reloadTimer)
+            {
+                GameObject bullet = bullets.Get();
+                if (bullet != null)
+                {
+                    bullet.transform.position = transform.position;
+                    bullet.transform.rotation = transform.rotation;
+                    bullet.SetActive(true);
+                    OnFire?.Invoke();
+                }
+            }
+        }
+    }
+}
