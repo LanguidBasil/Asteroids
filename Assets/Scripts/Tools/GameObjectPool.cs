73b9e5239f1cebfbf8ed68dd179ec3c20a3d0666
LanguidBasil <e.makunev@gmail.com>
2021-10-16 12:18:46 +1000

    Created GameObjectPool

---
+++ Assets/Scripts/Tools/GameObjectPool.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace Project.Tools
+{
+    public class GameObjectPool
+    {
+        private readonly int maxSize;
+        private GameObject[] pool;
+
+        public GameObjectPool(int maxSize, GameObject prototype)
+        {
+            this.maxSize = maxSize;
+            this.pool = new GameObject[maxSize];
+
+            for (int i = 0; i < maxSize; i++)
+            {
+                pool[i] = Object.Instantiate(prototype);
+                pool[i].SetActive(false);
+            }
+        }
+
+        public GameObject Get()
+        {
+            for (int i = 0; i < maxSize; i++)
+                if (pool[i].activeInHierarchy)
+                    return pool[i];
+            return null;
+        }
+    }
+}
