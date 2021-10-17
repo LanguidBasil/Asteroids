62043e78e3bbdc5cbe642dede15380f7d2af04c1
LanguidBasil <e.makunev@gmail.com>
2021-10-17 13:56:26 +1000

    Created spawners on scene

--- Assets/Scripts/Managers/GameFlow.cs
+++ Assets/Scripts/Managers/GameFlow.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
 
+using Project.Core;
+using Project.Core.Conf;
 using Project.Core.ObjectTypes;
 
 namespace Project.Managers
 {
     public class GameFlow : MonoBehaviour
     {
+        [SerializeField]
+        private SceneInfoSO sceneInfo;
+        [SerializeField]
+        private Spawner BigAsteroidSpawner;
+        [SerializeField]
+        private Spawner MediumAsteroidSpawner;
+        [SerializeField]
+        private Spawner SmallAsteroidSpawner;
+        [Space(8)]
         [SerializeField]
         [Tooltip("Number of big asteroids at start")]
         private int asteroidsAtStart;
@@ -29,7 +40,25 @@ namespace Project.Managers
 
         public void CreateAsteroid(AsteroidType asteroid, Vector3 position, Quaternion rotation)
         {
+            switch (asteroid)
+            {
+                case AsteroidType.Big:
+                    BigAsteroidSpawner.Spawn(position, rotation);
+                    break;
+                case AsteroidType.Medium:
+                    MediumAsteroidSpawner.Spawn(position, rotation);
+                    break;
+                case AsteroidType.Small:
+                    SmallAsteroidSpawner.Spawn(position, rotation);
+                    break;
+                default:
+                    break;
+            }
+        }
 
+        private Vector3 RandomOffCameraPosition()
+        {
+            return default;
         }
     }
 }
