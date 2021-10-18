442bd29575e6a3fa8d7831e56cd8b021eb73e4db
LanguidBasil <e.makunev@gmail.com>
2021-10-19 09:24:38 +1000

    Added Menu and MousePosition to PlayerInput and IInputGiver

--- Assets/Scripts/Input/IInputGiver.cs
+++ Assets/Scripts/Input/IInputGiver.cs
@@ -1,7 +1,10 @@
+using UnityEngine;
+
 namespace Project.Input
 {
     public interface IInputGiver
     {
+        public Vector2 MousePosition { get; }
         public float Rotation { get; }
         public bool Acceleration { get; }
         public bool Fire { get; }
--- Assets/Scripts/Input/PlayerInputs.cs
+++ Assets/Scripts/Input/PlayerInputs.cs
@@ -6,11 +6,20 @@ namespace Project.Input
     [RequireComponent(typeof(PlayerInput))]
     public class PlayerInputs : MonoBehaviour, IInputGiver
     {
+        public Vector2 MousePosition { get; private set; }
+
         public float Rotation { get; private set; }
 
         public bool Acceleration { get; private set; }
 
         public bool Fire { get; private set; }
+
+        public bool Menu { get; private set; }
+
+        public void OnMousePosition(InputAction.CallbackContext context)
+        {
+            MousePosition = context.ReadValue<Vector2>();
+        }
 
         public void OnRotation(InputAction.CallbackContext context)
         {
@@ -25,6 +34,11 @@ namespace Project.Input
         public void OnFire(InputAction.CallbackContext context)
         {
             Fire = context.ReadValueAsButton();
+        }
+
+        public void OnMenu(InputAction.CallbackContext context)
+        {
+            Menu = context.ReadValueAsButton();
         }
     }
 }
