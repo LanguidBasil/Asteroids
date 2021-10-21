b4b79b997e2a260a3d8fb9f9019176f913ff56b9
LanguidBasil <e.makunev@gmail.com>
2021-10-21 11:44:08 +1000

    Lives on start are now operated by GameFlow

--- Assets/Scripts/Managers/Scorer.cs
+++ Assets/Scripts/Managers/Scorer.cs
@@ -5,20 +5,11 @@ namespace Project.Managers
 {
     public class Scorer : MonoBehaviour
     {
-        [SerializeField]
-        private int livesAtStart;
-
         public int Score { get; private set; }
         public int Lives { get; private set; }
 
         public event EventHandler OnScoreChanged;
         public event EventHandler OnLivesChanged;
-
-        private void Awake()
-        {
-            Lives = livesAtStart;
-            OnLivesChanged?.Invoke(this, null);
-        }
 
         public void AddScore(int amount)
         {
