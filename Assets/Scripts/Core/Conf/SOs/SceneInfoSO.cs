6aa2826c44a869bc8326f196921a8f1585529986
LanguidBasil <e.makunev@gmail.com>
2021-11-02 13:30:16 +1000

    Added spawner's names to SceneInfoSO

--- Assets/Scripts/Core/Conf/SOs/SceneInfoSO.cs
+++ Assets/Scripts/Core/Conf/SOs/SceneInfoSO.cs
@@ -5,8 +5,15 @@ namespace Project.Core.Conf.SO
     [CreateAssetMenu(fileName = "New Scene Info", menuName = "Project / Core / Scene Info")]
     public class SceneInfoSO : ScriptableObject
     {
+        [Header("Camera")]
         public string CameraBoundsGameObjectName;
         [Tooltip("Half the size of the collider")]
         public Vector2 CameraBoundsExtents;
+        [Header("Spawners")]
+        public string bigAsteroidSpawnerName;
+        public string mediumAsteroidSpawnerName;
+        public string smallAsteroidSpawnername;
+        public string playerSpawnerName;
+        public string ufoSpawnerName;
     }
 }
